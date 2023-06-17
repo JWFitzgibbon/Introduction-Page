41354b436e7406b41ab0e95560e9521dc904164a
JWFitzgibbon <123597843+JWFitzgibbon@users.noreply.github.com>
2023-06-17 17:57:27 +0100

    CSS changes
    Gave the header section a box shadow, couldn't quite put it into a UI Card component for some reason that escapes me at the moment.

--- heropage/src/Components/IntroContent/IntroContent.js
+++ heropage/src/Components/IntroContent/IntroContent.js
@@ -9,19 +9,23 @@ function IntroContent() {
           <h1>
             <strong>Here is a Big Title</strong>
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum
-          </p>
-          <Button>
-            <strong>Get Started</strong>
-          </Button>
-          <Button className="btn btn-light">Information</Button>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+          <div>
+            <Button>
+              <strong>Get Started</strong>
+            </Button>
+            <Button className="btn btn-light">Information</Button>
+          </div>
         </div>
       </header>
     </div>
